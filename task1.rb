f1696f70a40e0efaf8e93e233ccbdcf642e480c0
Flavio Avila <flavioavila@Flavios-MacBook-Air.local>
2022-01-04 11:30:31 -0300

    Melhorias nos comentários

--- task1.rb
+++ task1.rb
@@ -12,19 +12,32 @@
 # each element of array A is an integer within the range [−1,000,000..1,000,000].
 
 def solution(a)
-    #Trato a lista para ter elementos unicos, não negativos e ordenados do menos para maior para facilitar percorrer
-    lista_tratada = a.uniq.sort! { |a, b| a <=> b }.select { |n| n > 0 }
+    #Trato a lista para ter elementos unicos, não negativos 
+    # e ordenados do menor para maior para facilitar percorrer e filtrando números maiores 1000000
+    lista_tratada = a.uniq
+                     .sort! { |a, b| a <=> b }
+                     .select { |n| n > 0 && n <= 1000000 }
 
-    #Se o retorno da instrução acima foi um array vazio, significa que eram apenas números negativos, assim retorna 1
+    #Se o retorno da instrução acima foi um array vazio, 
+    # significa que eram apenas números negativos, assim retorna 1
     return 1 if lista_tratada.empty?
+    
+    #Considerando que N só tem positivos limitados a 100000
+    tamanho_lista = lista_tratada.size
+    if lista_tratada.size > 10000
+        tamanho_lista = 10000
+    end
 
-    #Percorre a lista tratada se baseando no range de 1 até o tamanho do array passado
-    for num in 1..lista_tratada.size
-        #Checa a existência do número na lista tratada, caso não exista esse é o retorno do menor número não negativo que não ocorre na lista
+    #Percorre a lista tratada se baseando no range de 1 
+    # até o tamanho do array passado
+    for num in 1..tamanho_lista || 100000
+        #Checa a existência do número na lista tratada, caso não exista esse é o retorno do menor 
+        # número não negativo que não ocorre na lista
         return num if !lista_tratada.include?(num)
     end
 
-    #Caso tenha chegado até aqui, significa que todos os números percorridos na lista estavam presentes, assim a não ocorrencia passar a ser do próximo ao "max"
+    #Caso tenha chegado até aqui, significa que todos os números percorridos 
+    # na lista estavam presentes, assim a não ocorrencia passar a ser do próximo número depois do "max"
     lista_tratada.max + 1
 end
 
