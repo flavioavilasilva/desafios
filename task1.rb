c756261f89352785c4e9316b10ffc2e328b9487c
Flavio Avila <flavioavila@Flavios-MacBook-Air.local>
2022-01-04 02:51:42 -0300

    Incluindo comentários no código

--- task1.rb
+++ task1.rb
@@ -12,17 +12,20 @@
 # each element of array A is an integer within the range [−1,000,000..1,000,000].
 
 def solution(a)
+    #Trato a lista para ter elementos unicos, não negativos e ordenados do menos para maior para facilitar percorrer
     lista_tratada = a.uniq.sort! { |a, b| a <=> b }.select { |n| n > 0 }
 
+    #Se o retorno da instrução acima foi um array vazio, significa que eram apenas números negativos, assim retorna 1
     return 1 if lista_tratada.empty?
 
-    maior_valor = lista_tratada.max
-    
+    #Percorre a lista tratada se baseando no range de 1 até o tamanho do array passado
     for num in 1..lista_tratada.size
+        #Checa a existência do número na lista tratada, caso não exista esse é o retorno do menor número não negativo que não ocorre na lista
         return num if !lista_tratada.include?(num)
     end
 
-    maior_valor + 1
+    #Caso tenha chegado até aqui, significa que todos os números percorridos na lista estavam presentes, assim a não ocorrencia passar a ser do próximo ao "max"
+    lista_tratada.max + 1
 end
 
 puts solution([1, 3, 6, 4, 1, 2])
